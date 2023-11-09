e6d94685ab79f65eda308d5803baf3063cc8ab26
JAISHREERAMSIYA <149413817+JAISHREERAMSIYA@users.noreply.github.com>
2023-11-09 08:55:46 +0530

    Update evalute postfix.c

--- evalute postfix.c
+++ evalute postfix.c
@@ -1,60 +1,65 @@
 #include<stdio.h>
-#include<string.h>
 #include<conio.h>
-#define MAX 30
-#define TRUE 1
-#define FALSE 0
-int top=-1;
-int stack[MAX];
-void push(char);
-char pop();
-void main()
-{
-char expr[MAX],t;
-int i,valid=TRUE;
-clrscr();
-printf("\n\t Enter an algebraic expression:");
-gets(expr);
-for(i=0;i<strlen(expr);i++)
-{
- if(expr[i]=='('||expr[i]=='{'||expr[i]=='[')
- push(expr[i]);
- if(expr[i]==')'||expr[i]=='}'||expr[i]==']')
- if(top==-1)/*Stack Empty*/
- valid=FALSE;
- else
- {t=pop();
- if(expr[i]==')'&&(t=='{'||t=='['))
- valid=FALSE;
- if(expr[i]=='}'&&(t=='('||t=='['))
- valid=FALSE;
- if(expr[i]==']'&&(t=='('||t=='{'))
- valid=FALSE;
-}
-}
-if(top>=0)
-valid=FALSE;
-if(valid==TRUE)
-printf("\n\t Valid Expression.");
-else
-printf("\n\t Invalid Expression");
-getch();
-}
-void push(char item)
-{
-if(top==(MAX-1))
-printf("\n\t Stack Overflow.");
-else
-{
- top=top+1;
- stack[top]=item;
-}
-}
-char pop()
-{
-if(top==-1)
-printf("\n\t Stack Underflow.");
-else
-return((stack[top--]));
+int stack[20];
+int top = -1;
 
+void push(int x)
+{
+    stack[++top] = x;
+}
+
+int pop()
+{
+    return stack[top--];
+}
+
+int main()
+{
+    char exp[20];
+    char *e;
+    int n1,n2,n3,num;
+    printf("Enter the expression :: ");
+    scanf("%s",exp);
+    e = exp;
+    while(*e != '\0')
+    {
+        if(isdigit(*e))
+        {
+            num = *e - 48;
+            push(num);
+        }
+        else
+        {
+            n1 = pop();
+            n2 = pop();
+            switch(*e)
+            {
+            case '+':
+            {
+                n3 = n1 + n2;
+                break;
+            }
+            case '-':
+            {
+                n3 = n2 - n1;
+                break;
+            }
+            case '*':
+            {
+                n3 = n1 * n2;
+                break;
+            }
+            case '/':
+            {
+                n3 = n2 / n1;
+                break;
+            }
+            }
+            push(n3);
+        }
+        e++;
+    }
+    printf("\nThe result of expression %s  =  %d\n\n",exp,pop());
+    return 0;
+getch();
 }
